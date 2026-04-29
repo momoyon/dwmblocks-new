9218c01581a2d21ab182101d181444794783705e
ahmedsamyh <ahmedsamyh10@gmail.com>
2026-04-29 18:08:36 +0500

    [config.def.h] Use volume.pipewire.sh instead of pulsewire one.

--- config.def.h
+++ config.def.h
@@ -44,7 +44,7 @@ static const char delimiter[] = { ' ', ' ', ' ', DELIMITERENDCHAR };
 static Block blocks[] = {
 /*      pathu                           pathc                           interval        signal */
         { PATH("calendar.sh"),          NULL,                           30,             1},
-        { PATH("volume.sh"),            PATH("volume_button.sh"),       0,              2},
+        { PATH("volume.pipewire.sh"),            PATH("volume_button.sh"),       0,              2},
         { PATH("cpu_temp.sh"),          PATH("cpu_temp_button.sh"),     1,              4},
         { PATH("battery.sh"),           PATH("battery_button.sh"),      30,             3},
         { NULL } /* just to mark the end of the array */
